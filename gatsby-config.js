bb352578710ffa1fd83497899d65afb7a2c01c55
harshrpg <harshrp94@gmail.com>
2021-05-22 13:21:49 +0530

    Adding Material UI and the coins

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,8 @@ module.exports = {
   siteMetadata: {
     title: "horus",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: ["gatsby-plugin-gatsby-cloud", 
+  "gatsby-plugin-sharp", 
+  "gatsby-transformer-sharp",
+  "gatsby-plugin-material-ui"],
 };
