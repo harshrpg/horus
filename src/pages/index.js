ba80bb1af5ddd410841e5a624adfcc4f8a8ccfb0
harshrpg <harshrp94@gmail.com>
2021-05-25 09:55:46 +0530

    Adding more tokens

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import * as React from "react";
 import Logo from "../images/horus.png";
 import Container from "@material-ui/core/Container";
-import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,17 +18,42 @@ const IndexPage = () => {
       <main>
         <img src={Logo} alt="God Horus" width="100" height="100" />
         <title>Home Page</title>
-        <h1>
-          Horus
-        </h1>
+        <h1>Horus</h1>
       </main>
-      <Grid container spacing={2} className={classes.root} direction="row" justify="center"
-  alignItems="center">
-        <Grid item xs={12}>
+      <Grid
+        container
+        spacing={2}
+        className={classes.root}
+        direction="row"
+        justify="center"
+        alignItems="center"
+      >
+        <Grid item xs={6}>
           Shiba Inu
         </Grid>
-        <Grid item xs={12}>
+        <Grid item xs={6}>
           Bezoge
+        </Grid>
+        <Grid item xs={6}>
+          Noa Play
+        </Grid>
+        <Grid item xs={6}>
+          Zoo Token
+        </Grid>
+        <Grid item xs={6}>
+          SafeBank YES
+        </Grid>
+        <Grid item xs={6}>
+          Dick Foundation
+        </Grid>
+        <Grid item xs={6}>
+          TEN Finance
+        </Grid>
+        <Grid item xs={6}>
+          Wall Street Games
+        </Grid>
+        <Grid item xs={6}>
+          Deez Nuts
         </Grid>
       </Grid>
     </Container>
